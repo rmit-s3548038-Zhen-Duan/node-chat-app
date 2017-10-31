d6c013a7a72f4fe5da4a1c4ee5bcf339ac6ef40b
rmit-s3548038-Zhen-Duan <duan2439@gmail.com>
2017-11-01 10:59:38 +1100

    add form and show message

--- server/server.js
+++ server/server.js
@@ -20,10 +20,11 @@ io.on('connection', function(socket){
   socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
 
-  socket.on('createMessage', function(message){
+  socket.on('createMessage', function(message, callback){
     console.log('createMessage', message);
 
     io.emit('newMessage', generateMessage(message.from, message.text));
+    callback('This is from server');
   });
 
   socket.on('disconnect', function(){
